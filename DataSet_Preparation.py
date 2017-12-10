d100793550d10226663e8ad157b5f40589422afe
Berk <berkgulay.cs@gmail.com>
2017-12-10 21:56:03 +0300

    Data_Preparation-Resize

--- DataSet_Preparation.py
+++ DataSet_Preparation.py
@@ -1,1 +1,16 @@
-#author Berk Gülay
+#author Berk Gulay
+
+import Resize_Image as rs
+import os
+
+path = 'C:/Users/Berk/Desktop/BerkG/ComputerEngineering/Semester5/GitHub/DataSets/WarmthOfImage/0/'
+dest = 'C:/Users/Berk/Desktop/BerkG/ComputerEngineering/Semester5/GitHub/DataSets/WarmthOfImage/cropped/0'
+
+for filename in os.listdir(path):
+    rs.resize_image(200,
+                    path+filename,
+                    dest,
+                    filename)
+
+
+
