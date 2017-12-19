89040a4f27f4b3f4465f2cd85745e6b2e79b8412
Berk <berkgulay.cs@gmail.com>
2017-12-19 16:40:58 +0300

    Image_Describer Method Bodies added

--- Image_Description.py
+++ Image_Description.py
@@ -1,4 +1,23 @@
-#author Mert Surucuoglu
+#author Mert Surucuoglu, Berk Gulay
+
+def contrast():
+    pass
+
+def brightness():
+    pass
+
+
+def haze():
+    pass
+
+def color_hist():
+    pass
+
+def intensity_hist():
+    pass
+
+def sharpness():
+    pass
 
 
 
