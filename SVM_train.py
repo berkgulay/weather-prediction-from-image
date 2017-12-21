390a916dc7e51d6b63a2d93c44e66fe4cf27b07e
Berk <berkgulay.cs@gmail.com>
2017-12-21 22:59:10 +0300

    SVM train and test completed

--- SVM_train.py
+++ SVM_train.py
@@ -4,27 +4,73 @@ from sklearn import svm
 import numpy as np
 from sklearn.model_selection import KFold
 
+# This function seperates into sub classes for getting accuracy for each classes
+def separate_test_data(test_data, test_label):
+
+    cloudy_data = []
+    cloudy_label = []
+
+    sunny_data = []
+    sunny_label = []
+
+    rainy_data = []
+    rainy_label = []
+
+    snowy_data = []
+    snowy_label = []
+
+    foggy_data = []
+    foggy_label = []
+
+    for i in range(len(test_data)):
+        if int(test_label[i]) == 0:
+            cloudy_data.append(test_data[i])
+            cloudy_label.append(0)
+        elif int(test_label[i]) == 1:
+            sunny_data.append(test_data[i])
+            sunny_label.append(1)
+        elif int(test_label[i]) == 2:
+            rainy_data.append(test_data[i])
+            rainy_label.append(2)
+        elif int(test_label[i]) == 3:
+            snowy_data.append(test_data[i])
+            snowy_label.append(3)
+        elif int(test_label[i]) == 4:
+            foggy_data.append(test_data[i])
+            foggy_label.append(4)
+
+    return (    np.array(cloudy_data),np.array(cloudy_label),
+                np.array(sunny_data),np.array(sunny_label),
+                np.array(rainy_data),np.array(rainy_label),
+                np.array(snowy_data),np.array(snowy_label),
+                np.array(foggy_data), np.array(foggy_label)
+            )
+
+#K-fold splits into 10 and shuffles the indexes
+def k_fold(data,data_label,fold_num):
+    kf = KFold(n_splits=fold_num,shuffle=True)
+    kf.get_n_splits(data)
+
+    train_data = []
+    train_label = []
+    test_data = []
+    test_label = []
+    for train_index, test_index in kf.split(data):
+
+        train_data, test_data= data[train_index], data[test_index]
+        train_label, test_label = data_label[train_index], data_label[test_index]
+
+    return (train_data,
+            train_label,
+            test_data,
+            test_label)
+
+
 data = np.load('../DataSets/WarmthOfImage/SVM-DT/features_labels/features.npy')
 data_label = np.load('../DataSets/WarmthOfImage/SVM-DT/features_labels/labels.npy')
 data_label = np.reshape(data_label,(np.shape(data)[0],))
 
-# This function seperates into sub classes for getting accuracy for each classes
-def separate_data(_test_data, _test_label, cl):
-    separated_data = []
-    separated_label = []
-    for i in range(len(_test_data)):
-        if cl == int(_test_label[i]):
-            separated_data.append(_test_data[i])
-            separated_label.append(cl)
-    return np.array(separated_data), np.array(separated_label)
-
-#K-fold splits into 10 and shuffles the indexes
-kf = KFold(n_splits=10,shuffle=True)
-kf.get_n_splits(data)
-for train_index, test_index in kf.split(data):
-    #print("TRAIN:", train_index, "TEST:", test_index)
-    train_data, test_data= data[train_index], data[test_index]
-    train_label, test_label = data_label[train_index], data_label[test_index]
+train_data,train_label,test_data,test_label = k_fold(data,data_label,10)
 
 
 #try gamma = 3 or 2 and 0.2 or 0.1 , kernel= polynomial with degree 3,5,7,10 coef0 as 0,10,100
@@ -34,7 +80,29 @@ for train_index, test_index in kf.split(data):
 #try class_weight as default
 #try with no shrink (default with shrink change it)
 svc = svm.SVC(kernel='rbf',gamma=0.5,C=1.2,class_weight='balanced',max_iter=500,decision_function_shape='ovr',tol=0.001,cache_size=1000,probability=True)
+svc.fit(train_data,train_label)
 
-svc.fit()
 
-predictions = svc.predict()
+# makes a list for each class seperately to get each accuracy of them
+splitted_test = separate_test_data(test_data, test_label)
+validation_data = [ (splitted_test[t],splitted_test[t+1]) for t in range(0,10,2)]
+
+
+
+num_of_matches = 0
+num_test_data = 0
+# get an accuracy in here
+for j in range(len(validation_data)):
+    features_for_class = validation_data[j][0]  # features of a test image
+    labels_for_class = validation_data[j][1]  # label of a test image
+    predicted = svc.predict(features_for_class)
+
+    count_for_each = 0
+    for i in range(len(predicted)):
+        if predicted[i] == labels_for_class[i]:
+            count_for_each += 1
+            num_of_matches += 1
+        num_test_data += 1
+    print("Accuracy for", j, "class : ", count_for_each / len(predicted))
+print("General Accuracy : ", num_of_matches / num_test_data)
+print("0 is Cloudy\n1 is Sunny\n2 is Rainy\n3 is Snowy \n4 is Foggy\n")
