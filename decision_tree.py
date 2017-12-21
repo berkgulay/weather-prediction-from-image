0f4ee30ec85ce4a41bf28145efc7a55a99f14949
mertsurucu <mertsurucuogluu@gmail.com>
2017-12-21 13:11:42 +0300

    Simple changes

--- decision_tree.py
+++ decision_tree.py
@@ -29,27 +29,28 @@ for train_index, test_index in kf.split(data):
     train_label, test_label = data_label[train_index], data_label[test_index]
 
 train_data = train_data.reshape((len(train_data), -1))
+test_data = test_data.reshape((len(test_data), -1))
 
 estimator = DecisionTreeClassifier(max_leaf_nodes=50, random_state=10)
 estimator.fit(train_data, train_label)
 
-test_data = test_data.reshape((len(test_data), -1))
 
-vd = [seperateData(test_data, test_label, i) for i in range(5)]
-acc=0
-k=0
-for j in range(len(vd)):
-    v_data = vd[j][0]
-    v_label = vd[j][1]
+
+validation_data = [seperateData(test_data, test_label, i) for i in range(5)]
+maches=0
+total=0
+for j in range(len(validation_data)):
+    v_data = validation_data[j][0]
+    v_label = validation_data[j][1]
     y = estimator.predict(v_data)
     c=0
     for i in range(len(y)):
         if y[i]==v_label[i]:
             c+=1
-            acc+=1
-        k+=1
+            maches+=1
+        total+=1
     print("Acc for",j,": ",c/len(y))
-print(acc/k)
+print(maches/total)
 
 
 """for j in [0,1,2,3,4]:
