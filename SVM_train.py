8aea1b2ffc043ce0318607f302b499d3973668fc
Berk <berkgulay.cs@gmail.com>
2017-12-21 21:45:47 +0300

    SVM_train part with kfold

--- SVM_train.py
+++ SVM_train.py
@@ -1,9 +1,21 @@
 #author Berk Gulay
 
 from sklearn import svm
+import numpy as np
+from sklearn.model_selection import KFold
 
-train
+data = np.load('../DataSets/WarmthOfImage/SVM-DT/features_labels/features.npy')
+data_label = np.load('../DataSets/WarmthOfImage/SVM-DT/features_labels/labels.npy')
+data_label = np.reshape(data_label,(np.shape(data)[0],))
 
+
+#K-fold splits into 10 and shuffles the indexes
+kf = KFold(n_splits=10,shuffle=True)
+kf.get_n_splits(data)
+for train_index, test_index in kf.split(data):
+    #print("TRAIN:", train_index, "TEST:", test_index)
+    train_data, test_data= data[train_index], data[test_index]
+    train_label, test_label = data_label[train_index], data_label[test_index]
 
 #try gamma = 3 or 2 and 0.2 or 0.1 , kernel= polynomial with degree 3,5,7,10 coef0 as 0,10,100
 #try C as 4 and 2 and 0.5
