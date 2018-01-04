cfe14f7a8edda476935fe2e3bb40a66912c516f8
sametkalkan <32599629+sametkalkan@users.noreply.github.com>
2018-01-04 23:31:20 +0300

    bug fixed

--- predictImages.py
+++ predictImages.py
@@ -2,27 +2,33 @@
 import cv2
 import ImageDescriptor as id
 from keras.models import load_model
+from keras.preprocessing import image as image_utils
+from PIL import Image
+import PIL.ImageOps
+import tools as T
 
+#for i in ["0","1","2","3"]:
+#T.prepare_data_set("../cektiklerim/test/", "../cektiklerim/cropped100/", 100)
 
-#T.prepare_data_set("../berkfoto/deneme/0/","../berkfoto/cropped40berk/0/", 40)
-
-#T.image_to_matrix("../berkfoto/cropped40berk/",)
+#T.image_to_matrix("../myNewSet/cropped/", "../myNewSet/model/", 200)
 
 
 #id.create_features("../berkfoto/deneme", "../berkfoto/cropped40berk", "../berkfoto/feature")
 
-def predict_image_with_CNN(path):
+def predict_image_with_CNN(path, model):
     """
         predicts an image
         Returns:
             path of the image and its class
     """
-    # "../berkfoto/cropped/0/0ea78fdd-70f7-4c46-b88a-e3d61a767dbe.jpg"
-    model = load_model("modelsCNN/size100/trainedModelE20.h5")
-    img = cv2.imread(path)
+    img = image_utils.load_img(path, target_size=(100, 100))  # open an image
+    img = PIL.ImageOps.invert(img)  # inverts it
+    img = image_utils.img_to_array(img)  # converts it to array
+    img = img/255.0
     img = img.reshape(1, img.shape[0], img.shape[1], img.shape[2])
-    y = model.predict_classes(img, verbose=0)
-    return (path, y[0])
+
+    y = model.predict_classes(img, verbose=0, batch_size=1)
+    return path, T.classes[y[0]]
 
 def predict_image_with_RF(org_path, cropped_path, clf):
     """
@@ -31,17 +37,8 @@ def predict_image_with_RF(org_path, cropped_path, clf):
             path of the image and its class
     """
     feature = id.describe(org_path, cropped_path)
-    feature = feature.reshape(1,feature.shape[0])
     y = clf.predict(feature)
     return (org_path, y[0])
-
-
-
-
-
-
-
-
 
 
 
